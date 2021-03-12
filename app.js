2bef3c1aab2702766d28570fd5e236961eb086e5
Karen <karenyaa@live.co.uk>
2021-03-12 20:13:27 +0000

    created employee database with tables

---
+++ app.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+//const consoleTable = require("console.table");
+
+
+// Creating connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_db',
+});
+
+// Error if connection isn't made 
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+});
